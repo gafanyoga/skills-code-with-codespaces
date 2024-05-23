b47b5ba0e0d746db852086c3f39c53913495a80c
gafanyoga <n_lightenme@hotmail.com>
2024-05-23 23:58:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember()
+{
+    var member = new Object();
+    member.name = "John Doe";
+    member.age = 25;
+    member.skills = ["JavaScript", "Java", "C++"];
+    return member;
+}
